1a39918bee5ebbaed324f83d653c35afabce2b30
Franz-Niclas Muschter <franz-niclas.muschter@stusta.net>
2015-04-09 22:20:25 +0200

    jj magic

--- wiimote2shell.py
+++ wiimote2shell.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.4
 
 import socket
+import shlex
 import subprocess
 import argparse
 
@@ -10,12 +11,10 @@ def readconfig(path):
         conf = open(path, 'r').readlines()
         for line in conf:
             if line != '\n':
-                keys = line.split()
+                keys = shlex.split(line)
                 mapping[keys[0]] = keys[1:]
     except Exception as e:
-        print('Unable to parse config', path)
-        if DEBUG:
-            print(e)
+        print('Unable to parse config', path) # logger nehmen
         exit()
     return mapping
 
@@ -25,55 +24,59 @@ def do(action):
     elif action:
         # use Popen to run command in background
         subprocess.Popen(action)
+    else:
+        # ignore empty action
+        pass
 
 def check_buttons(buttons, mapping):
-    if buttons:
-        print(buttons)
+    """
+    in: [pressed button]
+    tests if each button is bound to some action
+    and executes it
 
+    TODO: combo actions?
+    """
     for button in buttons:
         if button in mapping.keys():
+            print(button, mapping[button])
             do(mapping[button])
 
 def main():
     parser = argparse.ArgumentParser(description='wiimote2shell remote control program')
     parser.add_argument('--device', '-d', help='wiimote mac address', required=True)
     parser.add_argument('--config', '-c', help='config file path', required=True)
-    parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
 
-    global DEBUG
-    DEBUG = args.debug
     mac = args.device
-    mapping = readconfig(args.config)
 
-    if DEBUG:
-        print(mapping)
-        print(mac)
+    # get {keyid => shellcmd list}
+    mapping = readconfig(args.config)
 
     # http://wiibrew.org/wiki/Wiimote#Buttons
     bitmap = [[0x01, 'LEFT' , 'TWO'],
               [0x02, 'RIGHT', 'ONE'],
-              [0x04, 'DOWN' , 'B'],
-              [0x08, 'UP'   , 'A'],
-              [0x10, 'PLUS' , 'MINUS'],
-              [0x80, None   , 'HOME']]
+              [0x04, 'DOWN',  'B'],
+              [0x08, 'UP',    'A'],
+              [0x10, 'PLUS',  'MINUS'],
+              [0x80, None,    'HOME']]
 
-    print('Press 1 + 2 on your Wiimote...')
+    print('Press 1 + 2 on your Wiimote for discover mode!')
     try:
+        # TODO: set or display timeout for connection attempt
         # sockets will magically be closed on programs termination, so don't worry
         s_in = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_L2CAP)
         s_out = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_L2CAP)
-        s_in.connect((mac, 0x13))
-        s_out.connect((mac, 0x11))
+        s_in.connect((mac, 0x13))  # wiimote receive port=19
+        s_out.connect((mac, 0x11)) # wiimote transmit port=17
     except OSError:
         print('Wiimote not found or bluetooth not available.')
         exit()
     print('Connected.')
 
     # request button only mode
-    s_out.send(bytes('\x52\x12\x00\x30', 'ascii'))
+    s_out.send(b'\x52\x12\x00\x30')
     # enable first player led
-    s_out.send(bytes('\x52\x11\x10', 'ascii'))
+    s_out.send(b'\x52\x11\x10')
 
     while True:
         try:
@@ -82,14 +85,15 @@ def main():
         except Exception as e:
             print(e)
             continue
+
         buttons = []
 
         # msg layout:    0  1  2  3
         #             (a1) 30 BB BB
         for i in [2, 3]:
             for hexv in bitmap:
-                # apply bitmask
-                if msg[i]&hexv[0]:
+                # test each bit, it represents a pressed button
+                if msg[i] & hexv[0]:
                     buttons.append(hexv[i-1])
 
         check_buttons(buttons, mapping)
